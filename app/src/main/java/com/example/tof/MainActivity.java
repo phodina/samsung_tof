a8219f2f02d3207e922ea3f768d49f206644d9ef
Petr Hodina <petr.hodina@luxonis.com>
2026-01-05 19:54:26 +0100

    src: Add missing Log import

--- app/src/main/java/com/example/tof/MainActivity.java
+++ app/src/main/java/com/example/tof/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.TextureView;
 
 import androidx.annotation.NonNull;
